b10ac864a8911a92005c6105d01373fadd422df3
BraidenKirkland <braidenkirkland1@gmail.com>
2023-12-12 11:34:29 -0800

    Hide taken pieces when showing reset modal

--- scripts/ButtonUIManager.js
+++ scripts/ButtonUIManager.js
@@ -2,13 +2,11 @@ export class ButtonUIManager {
     
     hideResetModalAndShowBoard() {
         document.querySelector('.confirm-reset').style.visibility = 'hidden';
-        document.getElementById('board').style.visibility = 'visible';
-        document.getElementById('reset').style.visibility = 'visible';
+        document.querySelector('.game').style.visibility = 'visible';
     }
 
     showResetModal() {
-        document.getElementById("board").style.visibility = "hidden";
-        document.getElementById('reset').style.visibility = "hidden";
+        document.querySelector(".game").style.visibility = "hidden";
         document.querySelector('.confirm-reset').style.visibility = "visible";
     }
 }
